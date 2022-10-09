64ae64defee6f95538d62443cb788dac70f58c09
Tushar K <tushars6016@gmail.com>
2022-10-09 15:07:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "verginiats"
 }
 
 variable "instance_type" {
   description = "instance type for ec2"
-  default     =  "t2.micro"
+  default     =  "t3.medium"
 }
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-jenkins-security-group"
+  default     = "my-eks-security-group"
 }
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
+  default     = "eks-master"
 }
 
 variable "ami_id" {
-  description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+  description = "AMI for AmazonLinux2 EC2 instance"
+  default     = "ami-026b57f3c383c2eec"
 }
